b1e4802db3659b3f0717bd43bd85dcc5a2445eb9
suda <c0a2107132@edu.teu.ac.jp>
2022-11-29 14:38:02 +0900

    練習5

--- ex02/calc.py
+++ ex02/calc.py
@@ -26,4 +26,14 @@ for num in range(9,-1,-1):
 
 entry = tk.Entry(root,justify = "right", width = 10,font = ("",40 ))
 entry.grid(row=0,column=0,columnspan = 3)
+
+operators = ["+","="]
+for ope in operators:
+    button = tk.Button(root, text = f"{ope}",width = 4,height = 2,font=("",30))
+    button.grid(row = r,column=c)
+    c+=1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
 root.mainloop()
