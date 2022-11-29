d5e983634ad0d5747d233d074dcc375ed18e8b52
abe <c0a21007be@edu.teu.ac.jp>
2022-11-29 16:07:05 +0900

    四則演算を可能にした。同時にボタン配置をリストで一括管理した

--- ex02/calc.py
+++ ex02/calc.py
@@ -20,25 +20,50 @@ def button_click(event):
 #ウィンドウ設定
 root = tk.Tk()
 root.title("電卓")
-root.geometry("300x500")
+root.geometry("400x500")
 
 #計算後に計算式を表示する欄
-keisansiki = tk.Entry(root,justify="right",width=20,font=("",10))
-keisansiki.grid(row = 0, column = 1, columnspan=3)
+keisansiki = tk.Entry(root,justify="right",width=40,font=("",10))
+keisansiki.grid(row = 0, column = 0, columnspan=3)
 #テキスト入力欄
 entry=tk.Entry(root,justify="right",width=10,font=("",40))
 entry.grid(row = 1, column = 0 , columnspan=3)
 
-#0-9のボタン表示
+#ボタン情報の一括管理
+bt = ["7","8","9","+",
+      "4","5","6","-",
+      "1","2","3","*",
+      "0","="]
+
 r,c = 2,0
-for i in range(9,-1,-1):
-    button = tk.Button(root,text=f"{i}",font=("",30),width=4,height=2)
+for key in bt:
+    button = tk.Button(root,text=f"{key}",font=("",30),width=4,height=2)
     button.bind("<1>",button_click)
     button.grid(row=r,column=c)
     c += 1
-    if (c%3 == 0):
+    if (c%4 == 0):
         r += 1
         c = 0
+
+
+"""
+#0-9のボタンと、右端に四則演算のボタンを表示
+r,c = 2,0
+bt = ["+","-","x","÷"]
+for i in range(9,-5,-1):
+    if(c%4 == 3):#cのカウントが右端に来たとき
+        button = tk.Button(root,text=f"{ope[j]}",font=("",30),width=4,height=2)
+        j += 1
+    else:
+        button = tk.Button(root,text=f"{i}",font=("",30),width=4,height=2)
+
+    button.bind("<1>",button_click)
+    button.grid(row=r,column=c)
+    c += 1
+    if (c%4 == 0):
+        r += 1
+        c = 0
+        i += 2
 
 #+と=のボタン配置
 operators = ["+","="]
@@ -47,9 +72,11 @@ for ope in operators:
     button.bind("<1>",button_click)
     button.grid(row=r,column=c)
     c += 1
-    if (c%3 == 0):
+    if (c%4 == 0):
         r += 1
         c = 0
+
+"""
 
 #実行
 root.mainloop()
