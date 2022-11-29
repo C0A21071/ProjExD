6f70cec0bacb210a9fed0cea90c3f388d1a99bb5
abe <c0a21007be@edu.teu.ac.jp>
2022-11-29 16:50:29 +0900

    完成

--- ex02/calc.py
+++ ex02/calc.py
@@ -32,7 +32,7 @@ keisansiki.grid(row = 0, column = 0, columnspan=4,sticky = tk.W+tk.E)
 entry=tk.Entry(root,justify="right",width=10,font=("",40))
 entry.grid(row = 1, column = 0 , columnspan=4,sticky=tk.W+tk.E)
 
-root.grid_rowconfigure(1,weight=1)
+#root.grid_rowconfigure(1,weight=1)
 
 #ボタン情報の一括管理
 bt = ["7","8","9","+",
@@ -42,45 +42,13 @@ bt = ["7","8","9","+",
 
 r,c = 2,0
 for key in bt:
-    button = tk.Button(root,text=f"{key}",font=("",30),width=4,height=2)
+    button = tk.Button(root,text=f"{key}",width=4,height=2,font=("",30))
     button.bind("<1>",button_click)
     button.grid(row=r,column=c)
     c += 1
     if (c%4 == 0):
         r += 1
         c = 0
-
-"""
-#0-9のボタンと、右端に四則演算のボタンを表示
-r,c = 2,0
-bt = ["+","-","x","÷"]
-for i in range(9,-5,-1):
-    if(c%4 == 3):#cのカウントが右端に来たとき
-        button = tk.Button(root,text=f"{ope[j]}",font=("",30),width=4,height=2)
-        j += 1
-    else:
-        button = tk.Button(root,text=f"{i}",font=("",30),width=4,height=2)
-
-    button.bind("<1>",button_click)
-    button.grid(row=r,column=c)
-    c += 1
-    if (c%4 == 0):
-        r += 1
-        c = 0
-        i += 2
-
-#+と=のボタン配置
-operators = ["+","="]
-for ope in operators:
-    button = tk.Button(root,text=f"{ope}",font=("",30),width=4,height=2)
-    button.bind("<1>",button_click)
-    button.grid(row=r,column=c)
-    c += 1
-    if (c%4 == 0):
-        r += 1
-        c = 0
-
-"""
 
 #実行
 root.mainloop()
