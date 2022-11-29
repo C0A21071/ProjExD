7af1965e40f156e74e35a7cf9dbc8629c504f7f5
abe <c0a21007be@edu.teu.ac.jp>
2022-11-29 14:25:42 +0900

    3.クリック時の反応を定義

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
+
 root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
@@ -8,10 +13,11 @@ root.geometry("300x500")
 r,c = 0,0
 for i in range(9,-1,-1):
     button = tk.Button(root,text=f"{i}",font=("",30),width=4,height=2)
+    button.bind("<1>",button_click)
     button.grid(row=r,column=c)
     c += 1
     if (c%3 == 0):
         r += 1
         c = 0
-        
+
 root.mainloop()
