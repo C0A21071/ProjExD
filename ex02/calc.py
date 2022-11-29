4de4373e44c88e2566b63239c659cf9a6b41c119
suda <c0a2107132@edu.teu.ac.jp>
2022-11-29 15:46:51 +0900

    数字の配置変更

--- ex02/calc.py
+++ ex02/calc.py
@@ -20,20 +20,23 @@ def button_click(event):
 
 #練習1
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("400x600")
 
 
 #練習2
 r = 1
-c = 0
+c = 2
 for num in range(9,-1,-1):
     button = tk.Button(root, text = f"{num}",width = 4,height = 2,font=("",30))
     button.grid(row = r,column=c)
     button.bind("<1>",button_click)
-    c+=1
-    if c%3 == 0:
+    c-=1
+    if c == -1:
         r += 1
+        c = 2
+    elif num == 0:
         c = 0
+        r = 4
 
 entry = tk.Entry(root,justify = "right", width = 10,font = ("",40 ))
 entry.grid(row=0,column=0,columnspan = 3)
@@ -45,9 +48,9 @@ for ope in operators:
     button.grid(row = r,column=c)
     button.bind("<1>",button_click)
     c+=1
-    if c%3 == 0:
-        r += 1
-        c = 0
+    if c%4 == 0:
+        r = 3
+        c = 5
 
 
 root.mainloop()
