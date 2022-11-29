ae8b4366468a385253046c9d39bf713cf2987f3a
abe <c0a21007be@edu.teu.ac.jp>
2022-11-29 14:22:09 +0900

    2. 0-9の表示を完成

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,4 +5,13 @@ root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
 
+r,c = 0,0
+for i in range(9,-1,-1):
+    button = tk.Button(root,text=f"{i}",font=("",30),width=4,height=2)
+    button.grid(row=r,column=c)
+    c += 1
+    if (c%3 == 0):
+        r += 1
+        c = 0
+        
 root.mainloop()
