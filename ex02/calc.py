2e7f9f31b26c4a4b9de1716219190c704f241297
abe <c0a21007be@edu.teu.ac.jp>
2022-11-29 15:27:17 +0900

    計算後上部にに計算式を表示する機能の作成

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,16 +2,17 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 #関数定義
-
 #ボタンを押した際の反応
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
-    if txt == "=":
+
+    if txt == "=": #=を押したときは式を計算する。
         line = entry.get()
         ans = eval(line)
         entry.delete(0,tk.END)
         entry.insert(tk.END,ans)
+        keisansiki.insert(tk.END,line)
     else:
         entry.insert(tk.END,txt)
     
@@ -21,12 +22,15 @@ root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")
 
+#計算後に計算式を表示する欄
+keisansiki = tk.Entry(root,justify="right",width=20,font=("",10))
+keisansiki.grid(row = 0, column = 1, columnspan=3)
 #テキスト入力欄
 entry=tk.Entry(root,justify="right",width=10,font=("",40))
-entry.grid(row = 0, column = 0 ,columnspan=3)
+entry.grid(row = 1, column = 0 , columnspan=3)
 
 #0-9のボタン表示
-r,c = 1,0
+r,c = 2,0
 for i in range(9,-1,-1):
     button = tk.Button(root,text=f"{i}",font=("",30),width=4,height=2)
     button.bind("<1>",button_click)
