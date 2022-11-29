5cbba98178f91912ac46029d4576560e54218a1c
abe <c0a21007be@edu.teu.ac.jp>
2022-11-29 14:40:37 +0900

    5.+と=の追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -29,5 +29,14 @@ for i in range(9,-1,-1):
         r += 1
         c = 0
 
+#+と=のボタン配置
+operators = ["+","="]
+for ope in operators:
+    button = tk.Button(root,text=f"{ope}",font=("",30),width=4,height=2)
+    button.grid(row=r,column=c)
+    c += 1
+    if (c%3 == 0):
+        r += 1
+        c = 0
 #実行
 root.mainloop()
