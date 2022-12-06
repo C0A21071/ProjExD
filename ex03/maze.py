31a4699a0bdc35a3eadc2d51b7066e378785c373
suda <c0a2107132@edu.teu.ac.jp>
2022-12-06 15:24:49 +0900

    練習問題完成

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,42 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+import maze_maker as mm
+
+
+def key_down(event):
+    global key 
+    key = event.keysym
+
+
+def key_up(event):
+    global key 
+    key = ""
+
+
+def main_proc():
+    global cx,cy,mx,my
+    if key =="Up":#上を押したとき
+        my-=1
+    elif key == "Down":#下を押したとき
+        my+=1
+    elif key == "Left":#左を押したとき
+        mx -=1
+    elif key == "Right":#右を押したとき
+        mx+=1
+
+    if maze_lst[mx][my] == 1:#移動先が壁の場合
+        if key =="Up":#上を押したとき
+            my+=1
+        elif key == "Down":#下を押したとき
+            my-=1
+        elif key == "Left":#左を押したとき
+            mx +=1
+        elif key == "Right":#右を押したとき
+            mx-=1
+    cx,cy = mx*100+50,my*100+50
+    canvas.coords("tori",cx,cy)
+    root.after(100,main_proc)
+
 
 if __name__ =="__main__":
     root = tk.Tk()
@@ -7,8 +44,20 @@ if __name__ =="__main__":
     canvas = tk.Canvas(root,width=1500,height=900,bg ="black" )
     canvas.pack()
 
-    tori = tk.PhotoImage(file="fig/8.png")
-    cx, cy = 300,400
+    maze_lst = mm.make_maze(15,9)
+    mm.show_maze(canvas,maze_lst)
+
+    tori = tk.PhotoImage(file="fig/1.png")
+
+    mx,my = 1,1
+    cx, cy = mx*100+50,my*100+50
     canvas.create_image(cx, cy, image=tori,tag = "tori")
+    
+    key = ""
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
+
+    main_proc()
+
 
     root.mainloop()
