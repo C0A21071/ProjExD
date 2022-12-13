04abd246e0565176d8a9c8ac1a1252f589662aec
suda <c0a2107132@edu.teu.ac.jp>
2022-12-13 16:16:40 +0900

    難易度の追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -36,6 +36,8 @@ def main():
     bomb_rct.centerx = random.randint(0,scrn_rct.width)
     bomb_rct.centery = random.randint(0,scrn_rct.height)
     scrn_sfc.blit(bomb_sfc, bomb_rct)
+
+    
     vx,vy = +1,+1
 
     while True:
@@ -43,7 +45,18 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            #F1,F2,F3キーを押すと爆弾の動く速さが変わる(ゲームの難易度変更)
+            if event.type == pg.KEYDOWN and event.key == pg.K_F1:
+                vx,vy = +2,+2
+            if event.type == pg.KEYDOWN and event.key == pg.K_F2:
+                vx,vy = +3,+3
+            if event.type == pg.KEYDOWN and event.key == pg.K_F3:
+                vx,vy = +4,+4
+            #SPACEキーを押すと爆弾が止まる
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                vx,vy = +0,+0
         key_dct = pg.key.get_pressed() #辞書型
+        #矢印キーか、wasdを押したときにこうかとんを移動させる
         if key_dct[pg.K_UP] or key_dct[pg.K_w]:
             tori_rct.centery -= 1
         if key_dct[pg.K_DOWN] or key_dct[pg.K_s]:
@@ -52,8 +65,9 @@ def main():
             tori_rct.centerx -= 1
         if key_dct[pg.K_RIGHT] or key_dct[pg.K_d]:
             tori_rct.centerx += 1
+
         if check_bound(tori_rct,scrn_rct) != (+1,+1):
-            #どこかしらはみ出ていたら
+            #こうかとんが画面からはみ出ないようにしている
             if key_dct[pg.K_UP] or key_dct[pg.K_w]:
                 tori_rct.centery += 1
             if key_dct[pg.K_DOWN] or key_dct[pg.K_s]:
@@ -70,10 +84,12 @@ def main():
         vy *= tate
 
         if tori_rct.colliderect(bomb_rct):
-            return
+            #return
+            tori_sfc = pg.image.load("fig/9.png")
         scrn_sfc.blit(bomb_sfc, bomb_rct)
         pg.display.update()
         clock.tick(1000)
+        
 
 
 if __name__ == "__main__":
