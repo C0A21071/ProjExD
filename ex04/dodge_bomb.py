9a9ee07569086f4ee2339304a839631c7b50b85b
abe <c0a21007be@edu.teu.ac.jp>
2022-12-13 14:09:32 +0900

    1.window

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,15 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん") #タイトルバーに文字列表示
+    scrn_sfc = pg.display.set_mode((1600,900)) #800x600の画面surfaceを生成
+
+    pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
