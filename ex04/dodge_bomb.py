59c695e7635127e839401f592b88c1ab007a0373
abe <c0a21007be@edu.teu.ac.jp>
2022-12-13 14:31:33 +0900

    4.koukatonidou

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,12 +1,48 @@
 import pygame as pg
 import sys
 
-def main():
-    pg.display.set_caption("逃げろ！こうかとん") #タイトルバーに文字列表示
-    scrn_sfc = pg.display.set_mode((1600,900)) #800x600の画面surfaceを生成
 
+def main():
+    clock =pg.time.Clock()
+    # 練習１
+    pg.display.set_caption("逃げろ！こうかとん")
+    scrn_sfc = pg.display.set_mode((1600, 900))
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
-    
+    pgbg_rct = pgbg_sfc.get_rect()
+
+    # 練習３
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+    # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
+    scrn_sfc.blit(tori_sfc, tori_rct) 
+
+    # 練習２
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+
+        key_dict = pg.key.get_pressed() #辞書型
+        if key_dict[pg.K_UP]:
+            tori_rct.centery -= 1 
+
+        if key_dict[pg.K_DOWN]:
+            tori_rct.centery += 1
+
+        if key_dict[pg.K_LEFT]:
+            tori_rct.centerx -= 1
+        if key_dict[pg.K_RIGHT]:
+            tori_rct.centerx += 1
+
+        scrn_sfc.blit(tori_sfc,tori_rct)
+
+
+        pg.display.update()
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
