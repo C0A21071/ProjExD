dac6d19e1cf58442fcc43649332662235e2a89bb
abe <c0a21007be@edu.teu.ac.jp>
2022-12-13 14:50:43 +0900

    6.

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 import sys
 
 
@@ -7,6 +8,7 @@ def main():
     # 練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
@@ -18,29 +20,42 @@ def main():
     # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
     scrn_sfc.blit(tori_sfc, tori_rct) 
 
+    # 練習５
+    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct) 
+
+
     # 練習２
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
-
-        key_dict = pg.key.get_pressed() #辞書型
-        if key_dict[pg.K_UP]:
-            tori_rct.centery -= 1 
-
-        if key_dict[pg.K_DOWN]:
+        # 練習4
+        key_dct = pg.key.get_pressed() # 辞書型
+        if key_dct[pg.K_UP]:
+            tori_rct.centery -= 1
+        if key_dct[pg.K_DOWN]:
             tori_rct.centery += 1
-
-        if key_dict[pg.K_LEFT]:
+        if key_dct[pg.K_LEFT]:
             tori_rct.centerx -= 1
-        if key_dict[pg.K_RIGHT]:
+        if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
+        scrn_sfc.blit(tori_sfc, tori_rct) 
 
-        scrn_sfc.blit(tori_sfc,tori_rct)
+        #練習6
+        vx, vy = +1, +1
+        bomb_rct.move_ip(vx,vy)
 
 
+        scrn_sfc.blit(bomb_sfc, bomb_rct) 
         pg.display.update()
         clock.tick(1000)
 
