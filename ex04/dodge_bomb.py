da41fc56f7517ec78e720334686fca5344627e3a
abe <c0a21007be@edu.teu.ac.jp>
2022-12-13 16:17:47 +0900

    スタート画面実装

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import random
 import sys
+import time
 
 
 def check_bound(obj_rct, scr_rct):
@@ -14,10 +15,10 @@ def check_bound(obj_rct, scr_rct):
         tate = -1
     return yoko, tate
 
-
-def main():
+#ゲーム自体を関数化
+def nigerokouka():
     clock =pg.time.Clock()
-    # 練習１
+        # 練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
     scrn_rct = scrn_sfc.get_rect()
@@ -42,6 +43,7 @@ def main():
     scrn_sfc.blit(bomb_sfc, bomb_rct) 
     vx, vy = +1, +1
 
+    
     # 練習２
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
@@ -79,16 +81,134 @@ def main():
         vx *= yoko
         vy *= tate
 
+        
+
         #練習8
         if tori_rct.colliderect(bomb_rct):
             return
-            
+
         pg.display.update()
         clock.tick(1000)
 
+def main():
+    #スタート画面の作成
+    start = pg.display.set_mode((200,100))
+    pg.display.set_caption("Start Window")
+
+    #ゲームスタート用ボタンの作成
+    button = pg.Rect(30,30,50,50)
+
+    #フォントの用意
+    font = pg.font.SysFont(None,25)
+
+    #テキストの設定
+    text = font.render("Start",True,(0,0,0))
+
+    while (1):
+        start.fill((0,0,0)) #画面を黒く塗る
+
+        #ボタンの設置
+        pg.draw.rect(start,(255,0,0),button)
+        #ボタンに文字を乗せる
+        start.blit(text,(40,45))
+
+        pg.display.update()
+
+        #バツが押されたら閉じる
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+            #スタートボタンが押されたらゲームを起動
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if button.collidepoint(event.pos):
+                    nigerokouka()
+    
+        
+
+    
+    #nigerokouka() #関数化した「逃げろこうかとん」を実行
+
+    """
+    clock =pg.time.Clock()
+    # 練習１
+    pg.display.set_caption("逃げろ！こうかとん")
+    scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
+    pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
+    pgbg_rct = pgbg_sfc.get_rect()
+
+    # 練習３
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+    # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
+    scrn_sfc.blit(tori_sfc, tori_rct) 
+
+    # 練習５
+    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct) 
+    vx, vy = +1, +1
+
+    
+    # 練習２
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        # 練習4
+        key_dct = pg.key.get_pressed() # 辞書型
+        if key_dct[pg.K_UP]:
+            tori_rct.centery -= 1
+        if key_dct[pg.K_DOWN]:
+            tori_rct.centery += 1
+        if key_dct[pg.K_LEFT]:
+            tori_rct.centerx -= 1
+        if key_dct[pg.K_RIGHT]:
+            tori_rct.centerx += 1
+        if check_bound(tori_rct, scrn_rct) != (+1, +1):
+            # どこかしらはみ出ていたら
+            if key_dct[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_dct[pg.K_DOWN]:
+                tori_rct.centery -= 1
+            if key_dct[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_dct[pg.K_RIGHT]:
+                tori_rct.centerx -= 1            
+        scrn_sfc.blit(tori_sfc, tori_rct) 
+
+        # 練習６
+        bomb_rct.move_ip(vx, vy)
+        scrn_sfc.blit(bomb_sfc, bomb_rct) 
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+
+        
+
+        #練習8
+        if tori_rct.colliderect(bomb_rct):
+
+            return
+
+        pg.display.update()
+        clock.tick(1000)
+"""
 
 if __name__ == "__main__":
     pg.init()
+    start = time.time()
     main()
+    end = time.time()
+    print(end - start)
     pg.quit()
     sys.exit()
