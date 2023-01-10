82b4864d9ee8c95a10182caad87306b45cb979e6
suda <c0a2107132@edu.teu.ac.jp>
2023-01-10 17:53:00 +0900

    追加機能迷路ゲーム

--- ex06/maiz.py
+++ ex06/maiz.py
@@ -5,7 +5,7 @@ def key_down(event):
     global key
     key = event.keysym
 
-
+#キーを離しても押された状態を保つために、キーを話したときの関数を削除した
 #def key_up(event):
  #   global key
   #  key = ""
@@ -13,25 +13,20 @@ def key_down(event):
 
 def main_proc():
     global cx, cy, mx, my
-    if key == "Up": my -= 1
-    if key == "Down": my += 1
-    if key == "Left": mx -= 1
-    if key == "Right": mx += 1
-    
-    if maze_lst[mx][my] == 1: # 移動先が壁だったら
-        if key == "Up": my += 1
-        if key == "Down": my -= 1
-        if key == "Left": mx += 1
-        if key == "Right": mx -= 1   
-    #if maze_lst[mx][my] != 1:
-     #   if maze_lst[mx][my+1] != 1:
-      #      if key == "Down": my += 1
-       #     if key == "Left": my += 1
-        #    if key == "Right": my += 1 
+    #wasdキーもしくわ矢印キーにより移動可能にしている
+    if key == "Up" or key == "w": my -= 1
+    if key == "Down" or key == "s": my += 1
+    if key == "Left" or key == "a": mx -= 1
+    if key == "Right" or key == "d": mx += 1
+    if (maze_lst[mx][my] == 1): #壁に衝突時にif文によりすり抜けないようにする
+        if key == "Up" or key == "w": my += 1
+        if key == "Down" or key == "s": my -= 1
+        if key == "Left" or key == "a": mx += 1
+        if key == "Right" or key == "d": mx -= 1
        
-    
     cx, cy = mx*40+20, my*40+20
     canvas.coords("kokaton", cx, cy)
+    
     root.after(150, main_proc)
 
 
@@ -49,7 +44,8 @@ if __name__ == "__main__":
     cx, cy = mx*30+30, my*30+30
     tori = tk.PhotoImage(file="fig/1.png")
     canvas.create_image(cx, cy, image=tori, tag="kokaton")
-    key = ""
+
+    #キーを離しても押された状態を保つようにしている
     root.bind("<KeyPress>", key_down)
     #root.bind("<KeyRelease>", key_up)
     main_proc()
